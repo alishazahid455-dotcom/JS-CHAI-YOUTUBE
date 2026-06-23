70e76170ed54b137e3e8bef0245a04521e2ef5c0
Alisha zahid <alisha.zahid455@gmail.com>
2026-06-23 08:31:33 +0000

    datatypes summary

---
+++ basic_js/datatypessummary.js
@@ -0,0 +1,14 @@
+/*datatypes on the bases of store in memory and access
+1-premitive (call by value)
+   number,string,boolean,null,undefined,gitint,symbol
+
+   2-non premitive types(call by reference)
+     arrays,objects,functions
+     cont arry1=["alis","ahmed",umair]array example
+     {
+     name="alisha"
+     age=18
+     } ju bi inn curly braces ma likhty inko object bolty inn curly braces ko
+      const myfun=function(){} ya function ki defination or usko variable myfun ma store ker diya
+       non premitive types ka return type function hi atta or function ka function object atta*/
+
