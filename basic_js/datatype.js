efa36403f72272729ba301da4c7aa653fd71c3d0
Alisha zahid <alisha.zahid455@gmail.com>
2026-06-23 06:51:21 +0000

    datatypes

---
+++ basic_js/datatype.js
@@ -0,0 +1,13 @@
+"use strict" //treat all js code as new verision
+//alert(3+3)//we rae using node.js not browser so it will not work here
+
+let name="alisha" //string datatype
+let age=18  //integer
+let isloggedin=false  //bolean
+//null is a standalone value it is also a datatype
+//symbol => uniqueness k liya use hutty
+//object
+console.log(typeof "alisha")//string type
+console.log(typeof null);//object type
+console.log(typeof undefined);//undefined hi aye gi iski type
+//ecma or mdn aik standard organization jis na javascript k liya standard define kiyea thy 
---
+++ basic_js/var.js
@@ -0,0 +1,11 @@
+const accountId=12345
+let accountEmail="alisha@374.com"
+var accountPassword="48756"
+accountCity="lhr"
+let accountState;
+
+
+accountEmail="alish65@gmail.com"
+accountPassword="4739"
+accountCity="eyru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
