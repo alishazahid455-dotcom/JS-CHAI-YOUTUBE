3bcf6e454f47e6c690ec5b20b7fdb14b28e27f53
Alisha zahid <alisha.zahid455@gmail.com>
2026-06-23 07:52:51 +0000

    discussion of conversionop

---
+++ basic_js/conversionop.js
@@ -0,0 +1,39 @@
+let score="33abc"
+//console.log(typeof score);
+
+let valueInNumber=Number(score) //strict kiya k score ki value number  my dy
+//console.log(typeof valueInNumber); //ya type bataye ga 
+//console.log(valueInNumber); //ya actual value bataye ga k value ha kaya
+
+//"33"=33
+//"33abc"=NaN(not a number iss na convert nai kiya number ma)
+//true=>1
+//null=>0
+let loggedIn="alisha"
+let booleanLoggedIn=Boolean(loggedIn)
+//console.log(booleanLoggedIn);
+
+// ************operation*********//
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" alisha"
+let str3=str1 + str2
+console.log(str3);
+console.log("1" + 2);//ager du values hu tu string hi banaye ga 
+console.log(1+"2"); 
+console.log("1" +2+2);//iss case ma string hi dy ga ku k start string sa huwa
+console.log(1+2+"2");// iss case ma shuru pay operation hu ga ku k int sa start hu raha
+
+
+
+
+
+
