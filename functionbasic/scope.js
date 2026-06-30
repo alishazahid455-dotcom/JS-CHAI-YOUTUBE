b09471c958cdabfeb14db7eedd3f29d823be30cb
Alisha zahid <alisha.zahid455@gmail.com>
2026-06-30 15:13:01 +0000

    scope discussion

---
+++ functionbasic/scope.js
@@ -0,0 +1,15 @@
+function one(){
+    username="alisha"
+    function two(){
+    const website="youtube"
+    console.log(username);
+    
+    }
+    //console.log(website);
+    two()
+}
+one()
+/****importent *****/
+/* isky ander main concept ya ha k one()wala function bra ha or two()wala choota choota fun nry waly
+k attribute access ker skta per bra wala chooty k nai ker skta same iss example ma bi yahi issue ha*/
+//yaha iss liya error aye ga ya nai chly ga jab tak console.log(website)ko comment nai kery
