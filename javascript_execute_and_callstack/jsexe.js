e673a8eb4f8ad4f82bf52b40997af33420957b4f
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-01 13:54:19 +0000

    interview instruc

---
+++ javascript_execute_and_callstack/jsexe.js
@@ -0,0 +1,8 @@
+//javascript execution text
+/*1-global execution context 2-functional execution context 3-eval(not needed much)
+js code run in two phases 1-memory creation phase 2-execution phase
+1-iss my sirf jaga allocate hutti 
+2-iss my operation bi 
+
+execution phase:sab sa phely jab code execute huna start hutta tu global execution sa start 
+hutta or isko this k ander rakhty */
