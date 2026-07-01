4abe3cb1cffc07249777c1b4a656234a2c48f683
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-01 07:26:55 +0000

    discussion of this and arrow

---
+++ functionbasic/thisandarrow.js
@@ -0,0 +1,38 @@
+const user={
+    username:"alisha",
+    price:99,
+    welcomemsg:function(){
+        console.log(`${this.username} welcome to website`);
+        console.log(this);
+        
+    }
+
+}
+// user.welcomemsg()
+// user.username="sam"
+// user.welcomemsg()   
+console.log(this );//this will give empty object ku k iss scope my khuch ha hi ni browser py global obj window ha
+
+
+function chai(){
+    let username="alisha"
+    console.log(this.username);
+    
+}
+//chai()//this will many values but will not give username values
+const chai=() => {
+let username="alisha"
+console.log(this.username);//iss ma ya undefined dy ga
+
+}
+chai()
+
+const addtwo=(n1,n2)=>{
+    return n1+n2
+}
+
+//implicit return 
+const addtwo=(n1,n2)=> (n1+n2)
+const addtwo=(n1,n2)=> ({username:"alisha"})//iss object ko return krny k liya parenthesis likhny hi hs
+
+console.log(addtwo(3,4)) 
