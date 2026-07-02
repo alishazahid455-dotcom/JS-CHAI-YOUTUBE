ff0ee5855a023ce34194b9b22b3c423673f51176
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-02 11:50:58 +0000

    dom

---
+++ dom/html.js
@@ -0,0 +1,19 @@
+//  <script>
+//     const div = document.createElement('div')
+//     console.log(div);
+//     div.className = "main"
+//     div.id = Math.round(Math.random() * 10 + 1)
+//     div.setAttribute("title", "generated title")
+//     div.style.backgroundColor = "green"
+//     div.style.padding = "12px"
+//     // div.innerText = "Chai aur code"
+//     const addText = document.createTextNode("Chai aur code")
+//     div.appendChild(addText)
+
+//     document.body.appendChild(div)
+// </script>
+
+
+
+// ya sara code js ka ha k ager hamny js pay koi new element add kerna hu or html pay na jana pery tu 
+// ham direct js ma assy create ker skty ha 
