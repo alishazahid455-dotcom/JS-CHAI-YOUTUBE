ad48af4b272f95a762c16aef609501dab0a7ef63
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-03 13:41:21 +0000

    even discussion

---
+++ events/event.js
@@ -0,0 +1,37 @@
+//Website par user jo bhi action karta hai, us action ko Event kehte hain.
+
+
+/*<script>
+document.getElementById('owl').onclick = function(){
+    alert("owl clicked")
+}
+
+document.getElementById('owl').addEventListener('click', function(){
+    alert("owl clicked again")
+})
+
+attachEvent()
+ jQuery - on
+
+document.getElementById('owl').addEventListener('click', function(e){
+ console.log(e)
+}, false)
+
+ type, timestamp, defaultPrevented
+ target, toElement, srcElement, currentTarget,
+ clientX, clientY, screenX, screenY
+ altkey, ctrlkey, shiftkey, keyCode
+
+ element.addEventListener("eventName", function () {   syntax of event
+     // Jo kaam karwana hai
+ });*/
+                   //</script>evet propgtaion
+
+/* document.getElementById('images').addEventListener('click', function(e){
+     console.log("clicked inside the ul");
+ })
+
+document.getElementById('owl').addEventListener('click', function(e){
+    console.log("owl clicked");
+    e.stopPropagation()
+ })*/
