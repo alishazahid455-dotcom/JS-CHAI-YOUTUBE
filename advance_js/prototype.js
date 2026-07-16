31632987916bd0fc1bba327a6f6d3772654744f8
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-16 08:06:48 +0000

    discussion of prototype

---
+++ advance_js/prototype.js
@@ -0,0 +1,49 @@
+//Prototype ek object hai jisse doosre objects methods aur properties inherit karte hain.
+let myhero = ["alisha", "rabiya"];
+
+let heropower = {
+    thor: "hammer",
+    spider: "sling",
+
+    getspiderpower: function () {
+        console.log(`Spider power is ${this.spider}`);
+    }
+};
+
+Object.prototype.alisha = function () {
+    console.log("alisha is present in all objects");
+};
+
+heropower.alisha();
+myhero.alisha();
+
+const user = {
+    name: "Alisha"
+};
+
+const Teacher = {
+    makeVideo: true
+};
+
+const TeachingSupport = {
+    isAvailable: false
+};
+
+const TASupport = {
+    makeAssignment: "JS assignment",
+    fullTime: true,
+    __proto__: TeachingSupport  //ya tareeqa bi ker skty
+};
+
+Teacher.__proto__ = user;// ya bi
+
+// Modern Syntax
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherusername = "sheza     ";
+
+String.prototype.trueLength = function () { // ya ham khud 
+    console.log(this.trim().length);
+};
+
+anotherusername.trueLength();
