fa133db86d147a8a28b9435744b85e51a974539b
Alisha zahid <alisha.zahid455@gmail.com>
2026-07-16 08:04:56 +0000

    discussion of ES modules

---
+++ advance_js/main.js
@@ -0,0 +1,12 @@
+import { PI, add } from "./ES_modulesmath.js";
+
+console.log(PI);
+console.log(add(2, 3));
+
+import { square } from "./ES_modulesmath.js";
+
+console.log(square(4)); // 16
+
+
+/*“ES Modules JavaScript ka built-in module system hai jisme hum code
+ ko multiple files mein divide karke export aur import ke through reuse karte hain.”*/
